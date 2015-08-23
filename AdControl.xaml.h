221af6db6d4c54e48c0910efb21ff654cdd31984
Duc Le <duchuule@users.noreply.github.com>
2015-08-23 03:58:52 +0000

    add banner ads

--- AdControl.xaml.h
+++ AdControl.xaml.h
@@ -14,5 +14,8 @@ namespace VBA10
 	{
 	public:
 		AdControl();
+	private:
+		void MSAdControl_ErrorOccurred(Platform::Object^ sender, Microsoft::Advertising::WinRT::UI::AdErrorEventArgs^ e);
+		void MSAdControl_AdRefreshed(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
 	};
 }
