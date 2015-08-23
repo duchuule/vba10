5c6ebf54aa2ea24c5c553bd22ee8669ae2e5b5a1
illidanx <illidanx@6295e596-52e5-47b5-b194-36d7faa446e6>
2015-08-23 03:58:52 +0000

    add banner ads

--- trunk/AdControl.xaml.cpp
+++ trunk/AdControl.xaml.cpp
@@ -18,10 +18,34 @@ using namespace Windows::UI::Xaml::Data;
 using namespace Windows::UI::Xaml::Input;
 using namespace Windows::UI::Xaml::Media;
 using namespace Windows::UI::Xaml::Navigation;
+using namespace Windows::UI::Core;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
 AdControl::AdControl()
 {
 	InitializeComponent();
+
+
+}
+
+
+void AdControl::MSAdControl_ErrorOccurred(Platform::Object^ sender, Microsoft::Advertising::WinRT::UI::AdErrorEventArgs^ e)
+{
+	this->Dispatcher->RunAsync(CoreDispatcherPriority::Normal, ref new DispatchedHandler([this]()
+	{
+		MSAdControl->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+		AdDuplexAdControl->Visibility = Windows::UI::Xaml::Visibility::Visible;
+		
+	}));
+}
+
+
+void AdControl::MSAdControl_AdRefreshed(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	this->Dispatcher->RunAsync(CoreDispatcherPriority::Normal, ref new DispatchedHandler([this]()
+	{
+		AdDuplexAdControl->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+		MSAdControl->Visibility = Windows::UI::Xaml::Visibility::Visible;
+	}));
 }
