56239912255f12b66cc3b5e593ba752a8520c2c5
illidanx <illidanx@6295e596-52e5-47b5-b194-36d7faa446e6>
2015-08-25 01:12:07 +0000

    change ad control to better behave

--- trunk/AdControl.xaml.h
+++ trunk/AdControl.xaml.h
@@ -17,5 +17,6 @@ namespace VBA10
 	private:
 		void MSAdControl_ErrorOccurred(Platform::Object^ sender, Microsoft::Advertising::WinRT::UI::AdErrorEventArgs^ e);
 		void MSAdControl_AdRefreshed(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void AdDuplexAdControl_AdCovered(Platform::Object^ sender, AdDuplex::Banners::Core::AdCoveredEventArgs^ e);
 	};
 }
